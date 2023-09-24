49d68a63834faf8694753f9a4828fa390677b51a
Imran Hussain <imranhussain399@gmail.com>
2023-09-24 11:20:25 +0500

    Modify placeShip method to avoid overlapping of the ships on th board and also update 2D board array on valid placement of the ship

--- dom.js
+++ dom.js
@@ -1,3 +1,7 @@
+/* eslint-disable import/extensions */
+// import GameBoard from "./gameBoard.js";
+import player from "./player.js";
+
 export default (function domManipulation() {
   function resetGrid(tableName) {
     if (tableName === "your-table") {
@@ -12,20 +16,21 @@ export default (function domManipulation() {
     }
   }
 
-  function createShip(shipClass, length) {
+  function createShip(shortName, length) {
     const ship = document.createElement("div");
     ship.style.position = "absolute";
     ship.style.top = "0";
     ship.style.left = "0";
     ship.style.width = `${2 * length}vw`;
-    ship.style.heigth = "2vw";
+    ship.style.height = "2vw";
 
     ship.classList.add("moving");
     ship.setAttribute("data-width", `${2 * length}vw`);
     ship.setAttribute("data-height", "2vw");
     ship.setAttribute("data-length", `${length}`);
     ship.setAttribute("data-orient", "x");
-    ship.classList.add(shipClass);
+    ship.classList.add(shortName);
+    ship.classList.add("ship");
     return ship;
   }
 
@@ -51,22 +56,25 @@ export default (function domManipulation() {
     }
   }
 
+  // eslint-disable-next-line consistent-return
   function placeShip(shipNames) {
     if (shipNames.length === 0) return 0;
     const shipArr = shipNames.shift();
-    const shipName = shipArr[0];
-    const length = shipArr[1];
+    const shortName = shipArr[0];
+    const shipName = shipArr[1];
+    const length = shipArr[2];
 
-    const shipShortNames = {
-      Carrier: "c",
-      Distroyer: "d",
-      "Patrol Boat": "p",
-      Singleton: "s",
-    };
+    // const shipShortNames = {
+    //   Carrier: "c",
+    //   Distroyer: "d",
+    //   "Patrol Boat": "p",
+    //   Singleton: "s",
+    // };
     const shipMessage = document.querySelector(".ship-name");
     shipMessage.innerHTML = "Place ".concat(shipName).concat(" on the board");
     let isPlaced = false;
-    const ship = createShip(shipShortNames[shipName], length);
+
+    const ship = createShip(shortName, length);
     const shipPlacePage = document.querySelector(".ship-placement-page");
     shipPlacePage.appendChild(ship);
 
@@ -75,11 +83,14 @@ export default (function domManipulation() {
         const shipPageRect = shipPlacePage.getBoundingClientRect();
         if (e.target.matches(".table-cell")) {
           ship.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+          ship.style.border = "1rem dashed #5978f5";
           const currentCell = e.target;
           const cellRect = currentCell.getBoundingClientRect();
           ship.style.left = `calc(${cellRect.right}rem  - ${shipPageRect.left}rem - ${ship.clientWidth}rem - 1.5rem )`;
           ship.style.top = `calc(${cellRect.bottom}rem - ${shipPageRect.top}rem - ${ship.clientHeight}rem - 1rem)`;
         } else {
+          ship.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
+          ship.style.border = "none";
           ship.style.left = `${e.clientX - shipPageRect.left - ship.clientWidth}rem`;
           ship.style.top = `${e.clientY - shipPageRect.top - ship.clientHeight}rem`;
         }
@@ -95,28 +106,29 @@ export default (function domManipulation() {
     function dropShip(e) {
       if (e.target.matches(".table-cell")) {
         const currentCell = e.target;
-
         const row = parseInt(currentCell.getAttribute("data-row"), 10);
         const col = parseInt(currentCell.getAttribute("data-col"), 10);
-        const shipLen = parseInt(ship.getAttribute("data-length"), 10);
+        // const shipLen = parseInt(ship.getAttribute("data-length"), 10);
         if (ship.getAttribute("data-orient") === "x") {
-          if (col + 1 - shipLen >= 0) {
+          if (player.isValidPos(row, col, length, "x")) {
             const shipRect = ship.getBoundingClientRect();
             const shipPageRect = shipPlacePage.getBoundingClientRect();
             const shipX = shipRect.left - shipPageRect.left;
             const shipY = shipRect.top - shipPageRect.top;
-
             ship.style.left = `${shipX}rem`;
             ship.style.top = `calc(${shipY}rem - 1rem)`;
             isPlaced = true;
             ship.classList.add("placed");
+            ship.style.border = "2rem solid blue";
             ship.classList.remove("moving");
             shipPlacePage.removeEventListener("mousemove", dragShip);
             shipPlacePage.removeEventListener("click", dropShip);
+            // Ship placed successfully. Now update the 2D board array
+            player.updateBoard(row, col,shortName, length, "x");
             placeShip(shipNames.slice());
           }
         } else if (ship.getAttribute("data-orient") === "y") {
-          if (row + 1 - shipLen >= 0) {
+          if (player.isValidPos(row, col, length, "y")) {
             const shipRect = ship.getBoundingClientRect();
             const shipPageRect = shipPlacePage.getBoundingClientRect();
             const shipX = shipRect.left - shipPageRect.left;
@@ -126,9 +138,12 @@ export default (function domManipulation() {
             ship.style.top = `calc(${shipY}rem - 1rem)`;
             isPlaced = true;
             ship.classList.add("placed");
+            ship.style.border = "2rem solid blue";
             ship.classList.remove("moving");
             shipPlacePage.removeEventListener("mousemove", dragShip);
             shipPlacePage.removeEventListener("click", dropShip);
+            // Ship placed successfully. Now update the 2D board array
+            player.updateBoard(row, col, shortName,length, "y");
             placeShip(shipNames.slice());
           }
         }
