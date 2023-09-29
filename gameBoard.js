02b0183fbc257f5db69b6398f4919ea229f8d54e
Imran Hussain <imranhussain399@gmail.com>
2023-09-29 10:34:29 +0500

    Modify autoFillShipsBoard method for ai to place ships at least one cell apart from each other to make auto placing more logical

--- gameBoard.js
+++ gameBoard.js
@@ -2,6 +2,7 @@ export default class GameBoard {
   constructor() {
     this.allSunk = false;
     this.board = [];
+    this.shipsCoord = {};
   }
 
   buildBoard() {
@@ -41,74 +42,184 @@ export default class GameBoard {
     }
     if (orient === "y") {
       for (let i = row; i > row - length; i -= 1) {
+        // console.log("i, col, shipName", i, col, shipName);
         this.board[i][col] = shipName;
       }
     }
-    console.log(this.board);
+    // Save this ship's coordinates
+    this.updateShipsCoord(row, col, shipName, length, orient);
   }
 
-  #getStartIndex(n, orientation) {
+  updateShipsCoord(row, col, shipName, length, orient) {
+    if (orient === "x") {
+      const ship = {};
+      ship.start = [row, col + 1 - length];
+      ship.end = [row, col];
+      ship.orient = orient;
+      ship.length = length;
+      this.shipsCoord[shipName] = ship;
+    }
+    if (orient === "y") {
+      const ship = {};
+      ship.start = [row + 1 - length, col];
+      ship.end = [row, col];
+      ship.orient = orient;
+      ship.length = length;
+      this.shipsCoord[shipName] = ship;
+    }
+  }
+
+  #getStartIndex(length, orient) {
     let indexFound = false;
     let row;
     let col;
     let cell;
-
-    if (orientation === "h") {
+    if (orient === "x") {
       while (!indexFound) {
         row = Math.floor(Math.random() * 10);
-        col = Math.floor(Math.random() * (10 - (n - 1)));
+        // Add offset = length - 1 in col to check prev n-1 cells as empty
+        col = length - 1 + Math.floor(Math.random() * (10 - (length - 1)));
         cell = this.board[row][col];
-        // Check if index position on board is empty and next 'n' cells are empty horizontally
+        // Check if current cell is empty and prev 'n-1' cells are empty horizontally
         if (cell === "empty") {
           indexFound = true;
-          for (let i = 1; i < n - 1; i += 1) {
-            if (this.board[row][col + i] !== "empty") {
+          for (let i = 1; i < length; i += 1) {
+            if (this.board[row][col - i] !== "empty") {
               indexFound = false;
               break;
             }
           }
         }
+
+        // One additional check after index is found is to make sure there is atleast one cell empty between consecutive ships in order to make auto ships placement more logical and less random.
+        if (indexFound) {
+          // Check if ship's all cells are away from corner rows and columns
+          if (row + 1 <= 9 && row - 1 >= 0 && col + 1 <= 9 && col - length - 1 >= 0) {
+            if (this.board[row][col + 1] !== "empty") {
+              indexFound = false;
+            }
+            if (this.board[row][col - length] !== "empty") {
+              indexFound = false;
+            }
+            for (let i = 0; i < length; i += 1) {
+              if (
+                this.board[row + 1][col - i] !== "empty" ||
+                this.board[row - 1][col - i] !== "empty"
+              )
+                indexFound = false;
+            }
+          }
+          if (row === 0) {
+            for (let i = 0; i < length; i += 1) {
+              if (this.board[row + 1][col - i] !== "empty") {
+                indexFound = false;
+              }
+            }
+          }
+          if (row === 9) {
+            for (let i = 0; i < length; i += 1) {
+              if (this.board[row - 1][col - i] !== "empty") {
+                indexFound = false;
+              }
+            }
+          }
+          if (col === 9) {
+            if (this.board[row][col - length] !== "empty") {
+              indexFound = false;
+            }
+          }
+          if (col - length === 0) {
+            if (this.board[row][col + 1] !== "empty") {
+              indexFound = false;
+            }
+          }
+        }
       }
-    } else if (orientation === "v") {
+    } else if (orient === "y") {
       while (!indexFound) {
-        row = Math.floor(Math.random() * (10 - (n - 1)));
+        // Add offset = length - 1 in row to check prev n-1 cells as empty
+        row = length - 1 + Math.floor(Math.random() * (10 - (length - 1)));
         col = Math.floor(Math.random() * 10);
         cell = this.board[row][col];
-        // Check if index position on board is empty and next 'n' cells are empty vertically
+        // Check if current cell is empty and prev 'n-1' cells are empty vertically
         if (cell === "empty") {
           indexFound = true;
-          for (let i = 1; i < n - 1; i += 1) {
-            if (this.board[row + i][col] !== "empty") {
+          for (let i = 1; i < length; i += 1) {
+            if (this.board[row - i][col] !== "empty") {
               indexFound = false;
               break;
+            }
+          }
+        }
+        // One additional check after index is found is to make sure there is atleast one cell empty between consecutive ships in order to make auto ships placement more logical and less random.
+        if (indexFound) {
+          // Check if ship's all cells are away from corner rows and columns
+          if (row + 1 <= 9 && row - length - 1 >= 0 && col + 1 <= 9 && col - 1 >= 0) {
+            if (this.board[row + 1][col] !== "empty") {
+              indexFound = false;
+            }
+            if (this.board[row - length][col] !== "empty") {
+              indexFound = false;
+            }
+            for (let i = 0; i < length; i += 1) {
+              if (
+                this.board[row - i][col + 1] !== "empty" ||
+                this.board[row - i][col - 1] !== "empty"
+              ) {
+                indexFound = false;
+              }
+            }
+          }
+          if (col === 0) {
+            for (let i = 0; i < length; i += 1) {
+              if (this.board[row - i][col + 1] !== "empty") {
+                indexFound = false;
+              }
+            }
+          }
+          if (col === 9) {
+            for (let i = 0; i < length; i += 1) {
+              if (this.board[row - i][col - 1] !== "empty") {
+                indexFound = false;
+              }
+            }
+          }
+          if (row === 9) {
+            if (this.board[row - length][col] !== "empty") {
+              indexFound = false;
+            }
+          }
+          if (row - length === 0) {
+            if (this.board[row + 1][col] !== "empty") {
+              indexFound = false;
             }
           }
         }
       }
     }
+
     return [row, col];
   }
 
-  placeShip(ship, orientation) {
-    const [row, col] = this.#getStartIndex(ship.length, orientation);
-    if (orientation === "h") {
-      for (let i = 0; i < ship.length; i += 1) {
-        this.board[row][col + i] = ship.name;
-      }
-    } else if (orientation === "v") {
-      for (let i = 0; i < ship.length; i += 1) {
-        this.board[row + i][col] = ship.name;
-      }
-    }
+  #placeSingleShip(ship, orient) {
+    const shipName = ship[0];
+    const length = ship[2];
+    // Use array destructuring for acccessing elements
+    const [row, col] = this.#getStartIndex(length, orient);
+    // Update ai board with this ship info
+    // console.log("row, col, shipName, length, ", row, col, shipName, length);
+    this.updateBoard(row, col, shipName, length, orient);
   }
 
-  autoPlace(shipsArr) {
-    const orientationArr = ["h", "v"];
-
+  autoFillShipsBoard(shipsArr) {
+    // console.log("ships array", shipsArr);
+    const orientArr = ["x", "y"];
     shipsArr.forEach((ship) => {
+      // chose orientation randomly
       const index = Math.floor(Math.random() * 2);
-      const orientation = orientationArr[index];
-      this.placeShip(ship, orientation);
+      const orient = orientArr[index];
+      // console.log("ship", ship);
+      this.#placeSingleShip(ship, orient);
     });
   }
 
@@ -126,3 +237,18 @@ export default class GameBoard {
     return cellStatus;
   }
 }
+const shipNames = [
+  ["c1", "Carrier", 4],
+  ["d1", "Distroyer", 3],
+  ["d2", "Distroyer", 3],
+  ["p1", "Patrol Boat", 2],
+  ["p2", "Patrol Boat", 2],
+  ["p3", "Patrol Boat", 2],
+  ["s1", "Singleton", 1],
+  ["s2", "Singleton", 1],
+  ["s3", "Singleton", 1],
+  ["s4", "Singleton", 1],
+];
+const b = new GameBoard();
+b.buildBoard();
+b.autoFillShipsBoard(shipNames);
