08c4065200abf9cab6725f9a489a137091e1e378
Imran Hussain <imranhussain399@gmail.com>
2023-09-29 21:16:43 +0500

    Modify ai.js moduleto add gameBoard class as property of the ai object

--- ai.js
+++ ai.js
@@ -2,18 +2,19 @@
 import GameBoard from "./gameBoard.js";
 import Ship from "./ship.js";
 
-export default function ai() {
+export default (function ai() {
   const newAI = {};
-  newAI.carrier = new Ship("carrier", 4);
-  newAI.distroyer1 = new Ship("distroyer1", 3);
-  newAI.distroyer2 = new Ship("distroyer2", 3);
-  newAI.patrolBoat1 = new Ship("patrolBoat1", 2);
-  newAI.patrolBoat2 = new Ship("patrolBoat2", 2);
-  newAI.patrolBoat3 = new Ship("patrolBoat3", 2);
-  newAI.singleton1 = new Ship("singleton1", 1);
-  newAI.singleton2 = new Ship("singleton2", 1);
-  newAI.singleton3 = new Ship("singleton3", 1);
-  newAI.singleton4 = new Ship("singleton4", 1);
-  Object.setPrototypeOf(newAI, new GameBoard());
+  newAI.c1 = new Ship("carrier", 4);
+  newAI.d1 = new Ship("distroyer1", 3);
+  newAI.d2 = new Ship("distroyer2", 3);
+  newAI.p1 = new Ship("patrolBoat1", 2);
+  newAI.p2 = new Ship("patrolBoat2", 2);
+  newAI.p3 = new Ship("patrolBoat3", 2);
+  newAI.s1 = new Ship("singleton1", 1);
+  newAI.s2 = new Ship("singleton2", 1);
+  newAI.s3 = new Ship("singleton3", 1);
+  newAI.s4 = new Ship("singleton4", 1);
+  // Object.setPrototypeOf(newAI, new GameBoard());
+  newAI.gameBoard = new GameBoard();
   return newAI;
-}
+})();
